659169ade0bd66c73cd36f654d4873d4d12c2dc0
Raju <raju@raju.gupta@blp.co.in>
2019-09-01 11:26:59 +0530

    first commit

---
+++ build/CMakeFiles/cmakeexp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmakeexp.dir/test.cpp.o"
+  "../bin/cmakeexp.pdb"
+  "../bin/cmakeexp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmakeexp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
